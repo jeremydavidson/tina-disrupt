81a2a7311eaab9ae8c57e5e3159eea5ce4bdabc5
Jeremy Davidson <jeremyfdavidson@gmail.com>
2024-08-02 11:12:51 -0700

    Add config to make sub-path deploy optional

--- tina/config.tsx
+++ tina/config.tsx
@@ -28,7 +28,7 @@ const config = defineConfig({
   build: {
     publicFolder: "public",
     outputFolder: "admin",
-    basePath: 'tina-disrupt',
+    basePath: process.env.IS_PATH_DEPLOY ? '' : process.env.PATH_NAME,
   },
   schema: {
     collections: [Page, Post, Author, Global],
