a2a52b777c82dd97109917c3ffaab7f9d93df0b1
Jeremy Davidson <jeremyfdavidson@gmail.com>
2024-08-02 10:44:35 -0700

    Add basePath again to fix admin/assets

--- tina/config.tsx
+++ tina/config.tsx
@@ -28,6 +28,7 @@ const config = defineConfig({
   build: {
     publicFolder: "public",
     outputFolder: "admin",
+    basePath: 'tina-disrupt',
   },
   schema: {
     collections: [Page, Post, Author, Global],
