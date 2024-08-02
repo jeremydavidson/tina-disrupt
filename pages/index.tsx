d501db64ebbaa3c9e681e3048fee439cc5d7f5db
Jeremy Davidson <jeremyfdavidson@gmail.com>
2024-08-02 00:27:07 -0700

    Change to v3 deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable no-unused-vars */
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
-
+console.log("Index to support static generation")
 function HomePage() {
   const router = useRouter();
+  console.log("Homepage to support static generation")
 
   useEffect(() => {
     router.replace('/home'); // Redirect immediately
